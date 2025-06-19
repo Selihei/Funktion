16e59e8cc913afeec63b626d52b1bce2a2788b0d
Selihei <selina.heinrichlp@gmail.com>
2025-06-19 14:44:40 +0200

    added file1 and file2

---
+++ Aufgabe1.dart
@@ -0,0 +1,13 @@
+int x = 5;
+
+void main(){
+printNumber(x);
+x = 10;
+printNumber(x);
+x = 33;
+printNumber(x);
+}
+
+void printNumber(int){
+print("$x");
+}
---
+++ Aufgabe2.dart
@@ -0,0 +1,14 @@
+String text ="Hello";
+
+void main(){
+triplePrint(text);
+text = "World";
+triplePrint(text);
+text = "!";
+triplePrint(text);
+}
+
+void triplePrint(string)
+{
+print("$text");
+}
